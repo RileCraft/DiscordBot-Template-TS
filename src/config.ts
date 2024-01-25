4f51a1ffbc153482b62ab292f43412a64132adb7
RileCraft <48475792+RileCraft@users.noreply.github.com>
2024-01-26 00:19:00 +0530

    Release Ready

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const PREFIX: Array<string> = ["Bot Prefix"];
 export const BOT_TOKEN: string = "Bot Token";
-export const OWNER_IDS: Array<string> = ["Bot Owner User ID"];
+export const OWNER_IDS: Array<string> = ["Bot Owner Discord ID"];
